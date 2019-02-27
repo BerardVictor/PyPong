249230bef55d96e7abaa179b4a3313dee4453d3f
Berard Victor <victorberardgh@gmail.com>
2019-02-27 16:56:12 -0300

    k
    j

--- pypong.py
+++ pypong.py
@@ -2,15 +2,11 @@ import sys
 import pygame
 from pygame.locals import *
 pygame.init()
-pygame.display.set_mode((640,680))
+fenetre=pygame.display.set_mode((640,680))
 pygame.display.set_caption('PyPongV0.0')
 black = (0,0,0)
 white = (255,255,255)
 terrain = pygame.Surface(fenetre.get_size())
-terrain.file(black)
-pygame.draw.rect(terrain, white, Rect((5,5),(630,470),2))
-pygame.draw.aaline(terrain, white, ((330,5),(330,475)))
-while True : 
-	draw(fenetre)
-	pygame.display.update()
-	fps.tick(60)
+terrain.fill(black)
+pygame.draw.rect(terrain, white, Rect((5,5),(630,470)),2)
+pygame.draw.aaline(terrain, white, (330,5),(330,475))
